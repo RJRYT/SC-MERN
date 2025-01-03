ccbb24f76e2b83de5d551ff58e552118d6ef833b
alenkdaniel <alenkdaniel77342@gmail.com>
2025-01-03 15:07:25 +0530

    Add and update terms file

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { ErrorPage } from "../pages/errorPage/ErrorPage";
 import { RootLayout } from "../layouts/RootLayout";
 import { UserLayout } from "../layouts/UserLayout";
+import Terms from "../pages/terms/Terms";
 // import { RegisterNow } from "../pages/root/RegisterNow";
 // import { SplashScreen } from "./../pages/root/SplashScreen";
 // import { Login } from "../pages/root/Login";
@@ -25,7 +26,12 @@ export const router = createBrowserRouter([
     //     path: "login",
     //     element: <Login />,
     //   },
+
     ],
+  },
+  {
+    path: "terms",
+    element:<Terms/>
   },
   {
     path: "user",
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,14 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-dark-purple': '#01011b',
+      },
+      screens: { 
+        'mobile-320': '320px', 
+      },
+    },
   },
   plugins: [],
 });
