643d1ca4a7990a2e065a6727ecb5920addde6ae1
RJRYT <robinjratr@gmail.com>
2025-01-04 15:01:17 +0530

    font updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-// Import font styles
-import './styles/fonts.css';
 import { ThemeProvider } from "@material-tailwind/react";
 
 createRoot(document.getElementById("root")).render(
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,11 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      
       fontFamily: {
-        titillium: ["'Titillium Web'", "sans-serif"],
+        titillium: ["Titillium Web", "sans-serif"],
       },
     },
   },
