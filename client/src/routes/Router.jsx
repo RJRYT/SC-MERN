15b7c42bba097ac0a439d9b7a570a8b43526d106
sherin joy <sherinjoy@sherins-MacBook-Air.local>
2025-01-04 10:03:37 +0530

    about

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { ErrorPage } from "../pages/errorPage/ErrorPage";
 import { RootLayout } from "../layouts/RootLayout";
 import { UserLayout } from "../layouts/UserLayout";
+import About from "../pages/root/About";
 // import { RegisterNow } from "../pages/root/RegisterNow";
 // import { SplashScreen } from "./../pages/root/SplashScreen";
 // import { Login } from "../pages/root/Login";
@@ -30,5 +31,11 @@ export const router = createBrowserRouter([
   {
     path: "user",
     element: <UserLayout />,
+
   },
+  {
+    path:"root",
+    element:<About/>
+  }
+ 
 ]);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,11 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-dark-purple': "#01011b", // Add the custom color
+      },
+    },
   },
   plugins: [],
 });
