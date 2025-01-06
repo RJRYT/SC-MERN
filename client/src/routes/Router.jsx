a8f6757378193d74bb8569bbca70ecbe99d5e67a
RJRYT <robinjratr@gmail.com>
2025-01-06 20:55:30 +0530

    code formating fix

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,39 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
-import { ErrorPage } from "../pages/errorPage/ErrorPage";
-import { RootLayout } from "../layouts/RootLayout";
-import { UserLayout } from "../layouts/UserLayout";
-// import { RegisterNow } from "../pages/root/RegisterNow";
-// import { SplashScreen } from "./../pages/root/SplashScreen";
-// import { Login } from "../pages/root/Login";
-import { FillProfile } from "../pages/root/FillProfile";
-import { CongratulationsPage } from "../pages/root/CongratulationPage";
-//import { CongratulationPage } from "../pages/root/CongratulationPage";
+import { FillProfile } from "../pages/auth/FillProfile";
+import { CongratulationsPage } from "../pages/auth/CongratulationPage";
 export const router = createBrowserRouter([
   {
-    path: "/",
-    errorElement: <ErrorPage />,
-    element: <RootLayout />,
-    children: [
-      //   {
-      //     path: "",
-      //     // path: "splashscreen",
-      //     element: <SplashScreen />,
-      //   },
-      //   {
-      //     path: "register",
-      //     element: <RegisterNow />,
-      //   },
-      //   {
-      //     path: "login",
-      //     element: <Login />,
-      //   },
-     
-    ],
-  },
-  {
-    path: "user",
-    element: <UserLayout />,
-  }, {
     path: "fillprofile",
     element: <FillProfile />,
   },
