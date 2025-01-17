86c569fdd699205d23a21680c735e14224b0812a
RJRYT <robinjratr@gmail.com>
2025-01-17 10:53:48 +0530

    helpcenter bug fix

--- client/src/pages/helpcenter/HeaderComponent.jsx
+++ client/src/pages/helpcenter/HeaderComponent.jsx
@@ -1,53 +1,39 @@
-import React, { useState } from 'react';
-import { IoIosArrowBack } from 'react-icons/io';
-
-import { RiEqualizer2Line } from "react-icons/ri";
-import { FaQ } from "react-icons/fa6";
-import FaqPage from './FaqPage';
-import ContactUs from './ContactUs';
-import Header from '../../components/header/Header';
-
-
+import React, { useState } from "react";
+import FaqPage from "./FaqPage";
+import ContactUs from "./ContactUs";
+import Header from "../../components/header/Header";
 
 function HeaderComponent() {
-  const [activeButton, setActiveButton] = useState('ongoing'); // Tracks the active button
+  const [activeTab, setActiveTab] = useState("faq");
 
   return (
+    <div className="flex w-full flex-col">
+      <Header title="HELP CENTER" showSearch showSettings />
 
-    <div className='flex w-full flex-col'>
-     
-
-<Header title="HELP CENTER" showSearch showSettings/>
-  
-
-
-      {/* Button Section */}
-      <div className="bg-btn-color w-full h-20 flex mt-4 rounded-full ">
-        {/* Ongoing Button */}
-
+      <div className="bg-gray-300 w-full h-20 flex">
         <button
-          className={`w-1/2 px-6 py-2 m-2 rounded-full ${
-            activeButton === 'ongoing'
-              ? 'bg-white text-btn-color text-bold border border-btn-color'
-              : 'bg-btn-color text-white text-bold'
+          className={`w-1/2 p-4 ${
+            activeTab === "faq"
+              ? "bg-gray-bg text-black border-b-2 border-blue-900"
+              : "bg-white text-gray-600"
           }`}
-          onClick={() => setActiveButton('ongoing')}
+          onClick={() => setActiveTab("faq")}
         >
-          Ongoing
+          FAQ
         </button>
-
-        {/* Completed Button */}
         <button
-          className={`w-1/2 px-6 py-2 m-2 rounded-full ${
-            activeButton === 'complete'
-              ? 'bg-white text-btn-color text-bold border border-btn-color'
-              : 'bg-btn-color text-white text-bold'
+          className={`w-1/2 p-4 ${
+            activeTab === "contact"
+              ? "bg-bg-gray-bg text-black border-b-2 border-blue-900"
+              : "bg-white text-gray-600"
           }`}
-          onClick={() => setActiveButton('complete')}
+          onClick={() => setActiveTab("contact")}
         >
-          Completed
+          CONTACT US
         </button>
       </div>
+
+      <div>{activeTab === "faq" ? <FaqPage /> : <ContactUs />}</div>
     </div>
   );
 }
